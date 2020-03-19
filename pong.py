34780e5dc0bca0c6b394b612c66f6417fe4e482c
mjkonko <mjkonko@gmail.com>
2020-03-19 13:14:19 +0100

    Window initialized

---
+++ pong.py
@@ -0,0 +1,17 @@
+# A Pong game constructed in Python 3
+# via freecodecamp
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("A pong game by @mjkonko")
+wn.bgcolor("white")
+wn.setup(width=1000, height=800)
+wn.tracer(0)
+
+
+#Main loop
+
+while True:
+    wn.update()
+
